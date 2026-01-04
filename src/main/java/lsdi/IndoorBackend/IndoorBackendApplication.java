d203e020a95f755bea93ba8091a6ed923506e98c
edson carlos <edson.sampaio@lsdi.ufma.br>
2026-01-04 20:44:27 -0300

    first commit

---
+++ src/main/java/lsdi/IndoorBackend/IndoorBackendApplication.java
@@ -0,0 +1,13 @@
+package lsdi.IndoorBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorBackendApplication.class, args);
+	}
+
+}
