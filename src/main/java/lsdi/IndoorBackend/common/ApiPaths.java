184a6a2a8b5520264154b91f4271a78d5f684b6d
edson carlos <edson.sampaio@lsdi.ufma.br>
2026-01-04 21:25:05 -0300

    feat: add apiPaths constants

---
+++ src/main/java/lsdi/IndoorBackend/common/ApiPaths.java
@@ -0,0 +1,9 @@
+package lsdi.IndoorBackend.common;
+
+public class ApiPaths {
+    public static final String OFFLINE  = "/coleta";
+    public static final String ONLINE = "/posicao";
+
+    public static final String ORGANIZATION = "/organizacao";
+    public static final String INFERENCE = "/dados-de-inferencia";
+}
