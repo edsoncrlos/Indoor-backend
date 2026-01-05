fe57d48b037555a776a84bd076928244e83f28a7
edson carlos <edson.sampaio@lsdi.ufma.br>
2026-01-05 20:51:08 -0300

    test: add InferenceIT

---
+++ src/test/java/lsdi/IndoorBackend/InferenceIT.java
@@ -0,0 +1,42 @@
+package lsdi.IndoorBackend;
+
+import lsdi.IndoorBackend.common.ApiPaths;
+import lsdi.IndoorBackend.dtos.IndoorInferenceDTO;
+import lsdi.IndoorBackend.entities.IndoorEnvironmentEntity;
+import lsdi.IndoorBackend.entities.OrganizationEntity;
+import lsdi.IndoorBackend.repositories.IndoorEnvironmentRepository;
+import lsdi.IndoorBackend.services.InferenceService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static lsdi.IndoorBackend.common.InferenceConstants.indoorEnvironmentsDTO;
+import static lsdi.IndoorBackend.common.OrganizationConstants.ROOT_ORG;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class InferenceIT {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private IndoorEnvironmentRepository indoorEnvironmentRepository;
+
+    @Test
+    public void addInference() {
+        Long idOrg = (restTemplate.postForEntity(ApiPaths.ORGANIZATION, ROOT_ORG, Long.class)).getBody();
+
+        IndoorInferenceDTO inferenceDTO = new IndoorInferenceDTO(
+                idOrg,
+                indoorEnvironmentsDTO
+        );
+
+        ResponseEntity<String> response = restTemplate.postForEntity(ApiPaths.INFERENCE, inferenceDTO, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isNotNull();
+    }
+}
---
+++ src/test/java/lsdi/IndoorBackend/common/InferenceConstants.java
@@ -0,0 +1,70 @@
+package lsdi.IndoorBackend.common;
+
+import lsdi.IndoorBackend.dtos.BeaconFingerprintDTO;
+import lsdi.IndoorBackend.dtos.IndoorEnvironmentDTO;
+
+import java.util.List;
+
+public class InferenceConstants {
+
+    public static List<IndoorEnvironmentDTO> indoorEnvironmentsDTO = List.of(
+            new IndoorEnvironmentDTO(
+                    "ets",
+                    List.of(
+                            new BeaconFingerprintDTO(
+                                    "AA:BB:CC:DD:EE:01",
+                                    -65.3f,
+                                    4.2,
+                                    1698765432000L,
+                                    17.64,
+                                    0.8f
+                            ),
+                            new BeaconFingerprintDTO(
+                                    "AA:BB:CC:DD:EE:02",
+                                    -67.0f,
+                                    3.8,
+                                    1698765435000L,
+                                    14.44,
+                                    0.7f
+                            ),
+                            new BeaconFingerprintDTO(
+                                    "AA:BB:CC:DD:EE:03",
+                                    -70.1f,
+                                    5.0,
+                                    1698765440000L,
+                                    25.0,
+                                    0.6f
+                            )
+                    )
+            ),
+            new IndoorEnvironmentDTO(
+                    "meet",
+                    List.of(
+                            new BeaconFingerprintDTO(
+                                    "11:22:33:44:55:01",
+                                    -60.0f,
+                                    3.0,
+                                    1698765500000L,
+                                    9.0,
+                                    1.0f
+                            ),
+                            new BeaconFingerprintDTO(
+                                    "11:22:33:44:55:02",
+                                    -62.5f,
+                                    3.5,
+                                    1698765505000L,
+                                    12.25,
+                                    0.9f
+                            ),
+                            new BeaconFingerprintDTO(
+                                    "11:22:33:44:55:03",
+                                    -64.0f,
+                                    4.0,
+                                    1698765510000L,
+                                    16.0,
+                                    0.85f
+                            )
+                    )
+            )
+    );
+}
