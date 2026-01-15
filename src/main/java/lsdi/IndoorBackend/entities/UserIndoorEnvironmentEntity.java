af11e66bf2c4092162af4e94eed1321d429870bf
edson carlos <edson.sampaio@lsdi.ufma.br>
2026-01-15 07:46:44 -0300

    fix: add constructor for UserIndoorEnvironmentEntity

--- src/main/java/lsdi/IndoorBackend/entities/UserIndoorEnvironmentEntity.java
+++ src/main/java/lsdi/IndoorBackend/entities/UserIndoorEnvironmentEntity.java
@@ -1,13 +1,16 @@
 package lsdi.IndoorBackend.entities;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 
 @Entity
 @Table(name = "TB_USER_INDOOR_ENVIRONMENT")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class UserIndoorEnvironmentEntity {
 
